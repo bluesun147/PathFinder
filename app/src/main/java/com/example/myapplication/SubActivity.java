2dbe68e80c88cc13ea3c03f1f5d34de0dd778907
HaeChan Kim <bluesun147@naver.com>
2021-11-23 19:26:17 +0900

    21.11.23 19:30

--- app/src/main/java/com/example/myapplication/SubActivity.java
+++ app/src/main/java/com/example/myapplication/SubActivity.java
@@ -1,12 +1,19 @@
 package com.example.myapplication;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import com.github.chrisbanes.photoview.PhotoView;
 // 지도 화면
@@ -25,6 +32,51 @@ public class SubActivity extends AppCompatActivity {
         ImageView menu = findViewById(R.id.menu); // 메뉴 버튼
 
         ImageView search = findViewById(R.id.search); // 검색 버튼
+
+        //Button push = findViewById(R.id.push); // 푸쉬 알림 테스트
+
+        //Button otherApp = findViewById(R.id.otherApp); // 다른 앱 테스트
+
+        /*otherApp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
+                //startActivity(launchIntent);
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.chrome");
+                if (launchIntent != null) {
+                    startActivity(launchIntent);
+                } else {
+                    Toast.makeText(SubActivity.this, "There is no package available in android", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        push.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap mLargeIcon =
+                        BitmapFactory.decodeResource(getResources(), R.drawable.mb4);
+
+                PendingIntent mPendingIntent = PendingIntent.getActivity(SubActivity.this, 0,
+                        new Intent(getApplicationContext(), SubActivity.class),
+                        PendingIntent.FLAG_CANCEL_CURRENT
+                );
+
+                NotificationCompat.Builder NBuilder =
+                        new NotificationCompat.Builder(SubActivity.this)
+                        .setSmallIcon(R.drawable.mb4)
+                        .setContentTitle("알림 제목!!")
+                        .setContentText("알림 내용!!")
+                        .setLargeIcon(mLargeIcon)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                        .setAutoCancel(true)
+                        .setContentIntent(mPendingIntent);
+
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                mNotificationManager.notify(0, NBuilder.build());
+
+            }
+        });*/
 
         menu.setOnClickListener(new View.OnClickListener() { // 메뉴 눌렀을 시 메뉴 페이지로 이동
             public void onClick(View view) {
